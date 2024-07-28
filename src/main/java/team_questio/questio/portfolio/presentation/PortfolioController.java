7899b2024dda69bee895a64e534bfc48f5981426
sanghee0820 <lsh277604@gmail.com>
2024-07-29 00:08:55 +0900

    feat: 생성된 Question 위치 반환 추가

--- src/main/java/team_questio/questio/portfolio/presentation/PortfolioController.java
+++ src/main/java/team_questio/questio/portfolio/presentation/PortfolioController.java
@@ -1,29 +1,28 @@
 package team_questio.questio.portfolio.presentation;
 
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import team_questio.questio.portfolio.presentation.dto.PortfolioRequest;
-import team_questio.questio.portfolio.service.PortfolioService;
+import team_questio.questio.portfolio.service.facade.PortfolioFacade;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/portfolio")
 @RequiredArgsConstructor
 public class PortfolioController {
-    private final PortfolioService portfolioService;
+    private final PortfolioFacade portfolioFacade;
 
     @PostMapping()
-    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Void> createPortfolio(@RequestBody PortfolioRequest request) {
-        var portfolioId = portfolioService.createPortfolio(request.toPortfolioParam());
+        var portfolioId = portfolioFacade.createPortfolio(request.toPortfolioParam());
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.created(URI.create("/api/v1/portfolio/" + portfolioId))
+                .build();
     }
 }
