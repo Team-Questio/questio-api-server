878212d9ca2f07120cf4de810a91f448765776b9
sanghee0820 <lsh277604@gmail.com>
2024-07-29 15:15:33 +0900

    feat: portfolioId로 quest 검색 기능 추가

--- src/main/java/team_questio/questio/portfolio/persistence/QuestRepository.java
+++ src/main/java/team_questio/questio/portfolio/persistence/QuestRepository.java
@@ -1,7 +1,9 @@
 package team_questio.questio.portfolio.persistence;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team_questio.questio.portfolio.domain.Quest;
 
 public interface QuestRepository extends JpaRepository<Quest, Long> {
+    List<Quest> findByPortfolioId(Long portfolioId);
 }
--- src/main/java/team_questio/questio/portfolio/service/QuestService.java
+++ src/main/java/team_questio/questio/portfolio/service/QuestService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import team_questio.questio.portfolio.persistence.QuestRepository;
 import team_questio.questio.portfolio.service.command.QuestCreateCommand;
+import team_questio.questio.portfolio.service.dto.QuestDetailInfo;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +17,14 @@ public class QuestService {
                 .map(QuestCreateCommand::toEntity)
                 .toList();
         questRepository.saveAll(quests);
+    }
+
+    public List<QuestDetailInfo> getQuests(Long portfolioId) {
+        var quests = questRepository.findByPortfolioId(portfolioId);
+
+        var response = quests.stream()
+                .map(QuestDetailInfo::from)
+                .toList();
+        return response;
     }
 }
