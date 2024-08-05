682408224a36d61abc50cdc35190b350dd5b63d3
sanghee0820 <lsh277604@gmail.com>
2024-08-05 23:44:14 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/team_questio/questio/common/persistence/BaseEntity.java
@@ -0,0 +1,23 @@
+package team_questio.questio.common.persistence;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private Long createdAt;
+    @LastModifiedDate
+    private Long updatedAt;
+}
