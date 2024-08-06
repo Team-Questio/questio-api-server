9f2f8c8bf1f7546ad7a5d7382d8f0e7bb85c366e
sanghee0820 <lsh277604@gmail.com>
2024-08-06 12:00:16 +0900

    feat: UserRole 추가

--- src/main/java/team_questio/questio/user/domain/User.java
+++ src/main/java/team_questio/questio/user/domain/User.java
@@ -10,4 +10,27 @@ import team_questio.questio.common.persistence.BaseEntity;
 public class User extends BaseEntity {
     private String username;
     private String password;
+    private Role role;
+
+    private User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public static User of(String username, String password, Role role) {
+        return new User(username, password, role);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
 }
