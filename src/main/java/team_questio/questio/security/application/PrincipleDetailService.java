e347340ef109eeba4d8e813c5145158ed488a892
sanghee0820 <lsh277604@gmail.com>
2024-08-06 12:01:44 +0900

    feat: PrincipleDetailService 추가

---
+++ src/main/java/team_questio/questio/security/application/PrincipleDetailService.java
@@ -0,0 +1,26 @@
+package team_questio.questio.security.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import team_questio.questio.common.exception.QuestioException;
+import team_questio.questio.common.exception.code.UserError;
+import team_questio.questio.user.persistence.UserRepository;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PrincipleDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> QuestioException.of(UserError.USER_NOT_FOUND));
+
+        return PrincipleDetails.of(user.getUsername(), user.getPassword(), user.getRole());
+    }
+}
