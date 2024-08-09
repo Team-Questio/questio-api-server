d02ab7166d5f00efd4085b06e53ce4d57e0113d2
sanghee0820 <lsh277604@gmail.com>
2024-08-09 20:14:27 +0900

    fix: 토큰에 UUID 및, 발급시간 추가

--- src/main/java/team_questio/questio/security/util/JWTTokenProvider.java
+++ src/main/java/team_questio/questio/security/util/JWTTokenProvider.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,8 @@ public class JWTTokenProvider {
     public String generateAccessToken(Long id, String username) {
         return Jwts.builder()
                 .setClaims(Map.of("id", id, "username", username))
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.accessTokenExpireIn()))
                 .signWith(SignatureAlgorithm.HS512, jwtProperties.accessTokenSecretKey())
                 .compact();
@@ -23,6 +26,8 @@ public class JWTTokenProvider {
     public String generateRefreshToken(Long id, String username) {
         return Jwts.builder()
                 .setClaims(Map.of("id", id, "username", username))
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.refreshTokenExpireIn()))
                 .signWith(SignatureAlgorithm.HS512, jwtProperties.refreshTokenSecretKey())
                 .compact();
