d19e859103f4d097a4d12067fc96fc0c6148b90b
sanghee0820 <lsh277604@gmail.com>
2024-08-09 22:43:49 +0900

    feat: 인증 코드 검증 로직 추가

--- src/main/java/team_questio/questio/security/application/AuthenticationService.java
+++ src/main/java/team_questio/questio/security/application/AuthenticationService.java
@@ -2,24 +2,33 @@ package team_questio.questio.security.application;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import team_questio.questio.common.exception.QuestioException;
-import team_questio.questio.common.exception.code.ServerError;
+import team_questio.questio.common.exception.code.AuthError;
+import team_questio.questio.infra.RedisUtil;
 import team_questio.questio.security.util.MailCertificationUtil;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private static final Long EMAIL_EXPIRE_MINUTE = 5L;
+    private static final Long EMAIL_CERTIFICATION_SUCCESS_EXPIRE_MINUTE = 10L;
+
+    private final RedisUtil redisUtil;
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
 
+    @Transactional
     public void sendEmail(String email) {
-        var htmlContent = createHtmlContent(MailCertificationUtil.generateCertificationNumber());
+        var code = MailCertificationUtil.generateCertificationNumber();
+        var htmlContent = createHtmlContent(code);
         MimeMessage message = mailSender.createMimeMessage();
 
         try {
@@ -29,13 +38,51 @@ public class AuthenticationService {
             helper.setText(htmlContent, true);
             mailSender.send(message);
         } catch (MessagingException e) {
-            throw QuestioException.of(ServerError.MAIL_SEND_FAILED);
+            throw QuestioException.of(AuthError.MAIL_SEND_FAILED);
         }
+
+        saveSecretCode(email, code);
+    }
+
+    @Transactional
+    public void verifyCode(final String email, final String code) {
+        var key = redisUtil.getEmailCertificationKey(email);
+        var savedCode = redisUtil.getData(key, String.class)
+                .orElseThrow(() -> QuestioException.of(AuthError.CERTIFICATION_CODE_EXPIRED));
+
+        deleteSecretCode(email);
+        if (!code.equals(savedCode)) {
+            throw QuestioException.of(AuthError.INVALID_CERTIFICATION_CODE);
+        }
+        saveSuccessCertificationInfo(email);
     }
 
     private String createHtmlContent(String code) {
         var context = new Context();
         context.setVariable("verificationCode", code);
         return templateEngine.process("email-certification-template", context);
+    }
+
+    private void saveSecretCode(String email, String code) {
+        var key = redisUtil.getEmailCertificationKey(email);
+        boolean failed = !redisUtil.setData(key, code, EMAIL_EXPIRE_MINUTE, TimeUnit.MINUTES);
+        if (failed) {
+            throw QuestioException.of(AuthError.MAIL_SEND_FAILED);
+        }
+    }
+
+    private void saveSuccessCertificationInfo(String email) {
+        var key = redisUtil.getEmailCertificationSuccessKey(email);
+        boolean failed = !redisUtil.setData(key, Boolean.TRUE, EMAIL_CERTIFICATION_SUCCESS_EXPIRE_MINUTE,
+                TimeUnit.MINUTES);
+
+        if (failed) {
+            throw QuestioException.of(AuthError.CERTIFICATION_ERROR);
+        }
+    }
+
+    private void deleteSecretCode(String email) {
+        var key = redisUtil.getEmailCertificationKey(email);
+        redisUtil.deleteData(key);
     }
 }
