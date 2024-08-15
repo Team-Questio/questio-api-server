b05851f71e6ce3ddb6af54efb16087c449ad0495
sanghee0820 <lsh277604@gmail.com>
2024-08-15 21:56:27 +0900

    refactor: 기본 form로그인 및 oauth 로그인 활용하도록 변경

--- src/main/java/team_questio/questio/security/config/FilterConfig.java
+++ src/main/java/team_questio/questio/security/config/FilterConfig.java
@@ -3,35 +3,16 @@ package team_questio.questio.security.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import team_questio.questio.security.filter.AuthenticationResultHandler;
-import team_questio.questio.security.filter.JWTAuthenticationFilter;
 import team_questio.questio.security.filter.JWTAuthorizationFilter;
 import team_questio.questio.security.util.JWTTokenProvider;
 
 @Configuration
 @RequiredArgsConstructor
 public class FilterConfig {
-    private final SecurityProperties securityProperties;
-
     @Bean
     public JWTAuthorizationFilter jwtAuthorizationFilter(
-            AuthenticationManager authenticationManager,
             JWTTokenProvider jwtTokenProvider
     ) {
-        return new JWTAuthorizationFilter(authenticationManager, jwtTokenProvider);
-    }
-
-    @Bean
-    public JWTAuthenticationFilter jwtAuthenticationFilter(
-            AuthenticationResultHandler authenticationResultHandler,
-            AuthenticationManager authenticationManager
-    ) {
-        var filter = new JWTAuthenticationFilter(authenticationManager);
-        filter.setFilterProcessesUrl(securityProperties.loginUrl());
-        filter.setAuthenticationSuccessHandler(authenticationResultHandler);
-        filter.setAuthenticationFailureHandler(authenticationResultHandler);
-
-        return new JWTAuthenticationFilter(authenticationManager);
+        return new JWTAuthorizationFilter(jwtTokenProvider);
     }
 }
