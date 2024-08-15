a248124e3db5ebbf55e8e185ccdfd02ea8c3eddc
sanghee0820 <lsh277604@gmail.com>
2024-08-15 21:53:11 +0900

    feat: Authorization 실패 시 redirect를 방지하기 위해 handler 추가

--- src/main/java/team_questio/questio/security/config/HandlerConfig.java
+++ src/main/java/team_questio/questio/security/config/HandlerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import team_questio.questio.security.application.JWTTokenService;
 import team_questio.questio.security.filter.AuthenticationResultHandler;
+import team_questio.questio.security.filter.AuthorizationFailureHandler;
 
 @Configuration
 public class HandlerConfig {
@@ -12,5 +13,10 @@ public class HandlerConfig {
             JWTTokenService jwtTokenService
     ) {
         return new AuthenticationResultHandler(jwtTokenService);
+    }
+
+    @Bean
+    public AuthorizationFailureHandler authorizationFailureHandler() {
+        return new AuthorizationFailureHandler();
     }
 }
---
+++ src/main/java/team_questio/questio/security/filter/AuthorizationFailureHandler.java
@@ -0,0 +1,16 @@
+package team_questio.questio.security.filter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+public class AuthorizationFailureHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+    }
+}
