306c2d531919f3058ec06c92cb2e328048dbee42
sanghee0820 <lsh277604@gmail.com>
2024-08-21 09:52:08 +0900

    feat: exception 처리 로직 추가

--- src/main/java/team_questio/questio/common/exception/GlobalExceptionHandler.java
+++ src/main/java/team_questio/questio/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package team_questio.questio.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +17,16 @@ public class GlobalExceptionHandler {
 
         var problemDetail = ProblemDetail.forStatusAndDetail(e.httpStatus(), e.message());
         problemDetail.setProperty(DEFAULT_ERROR_CODE, e.code());
+
+        return problemDetail;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleException(Exception e) {
+        log.error(e.getMessage(), e);
+
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+        problemDetail.setProperty(DEFAULT_ERROR_CODE, "E999");
 
         return problemDetail;
     }
