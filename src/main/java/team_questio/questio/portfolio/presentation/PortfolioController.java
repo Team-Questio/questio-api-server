73847fae01f3d0fd9e1f3af2938009307e620dbe
sanghee0820 <lsh277604@gmail.com>
2024-08-24 23:57:59 +0900

    feat: 포트폴리오 전체 조회 API 추가

--- src/main/java/team_questio/questio/portfolio/presentation/PortfolioController.java
+++ src/main/java/team_questio/questio/portfolio/presentation/PortfolioController.java
@@ -1,6 +1,7 @@
 package team_questio.questio.portfolio.presentation;
 
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -29,7 +30,6 @@ public class PortfolioController implements PortfolioApiController {
     public ResponseEntity<Void> createPortfolio(@RequestBody PortfolioRequest request,
                                                 Authentication authentication
     ) {
-
         Long userId = Long.valueOf(authentication.getPrincipal().toString());
         var command = request.toPortfolioParam(userId);
         var portfolioId = portfolioFacadeService.createPortfolio(command);
@@ -50,6 +50,20 @@ public class PortfolioController implements PortfolioApiController {
         return ResponseEntity.ok()
                 .body(response);
     }
+
+    @Override
+    public ResponseEntity<List<PortfolioResponse>> getPortfolios(Authentication authentication) {
+        Long userId = Long.valueOf(authentication.getPrincipal().toString());
+        var portfolios = portfolioFacadeService.getPortfolios(userId);
+
+        var response = portfolios.stream()
+                .map(PortfolioResponse::from)
+                .toList();
+        return ResponseEntity.ok()
+                .body(response);
+    }
+
+
 
     @PatchMapping("/quest/{questId}")
     public ResponseEntity<Void> updateFeedback(@PathVariable Long questId,
