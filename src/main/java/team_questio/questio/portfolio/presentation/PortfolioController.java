5884bbac4799c812d4e216ef8d7d1303c26a91d6
sanghee0820 <lsh277604@gmail.com>
2024-08-25 00:11:26 +0900

    chore: 코드 포멧 변경

--- src/main/java/team_questio/questio/portfolio/presentation/PortfolioController.java
+++ src/main/java/team_questio/questio/portfolio/presentation/PortfolioController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,11 +62,9 @@ public class PortfolioController implements PortfolioApiController {
                 .body(response);
     }
 
-
-
     @PatchMapping("/quest/{questId}")
     public ResponseEntity<Void> updateFeedback(@PathVariable Long questId,
-        @RequestBody FeedbackRequest request) {
+                                               @RequestBody FeedbackRequest request) {
 
         portfolioFacadeService.updateFeedback(questId, request.feedback());
         return ResponseEntity.ok().build();
