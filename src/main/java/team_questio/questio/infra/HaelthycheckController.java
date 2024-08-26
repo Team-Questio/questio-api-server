dd2464f18ab633d84d5cd8d8be92bb30b8a1585e
doyooon <ian661087@gmail.com>
2024-08-26 15:28:25 +0900

    feat HealthycheckController : healtycheck 용 컨트롤러 생성

---
+++ src/main/java/team_questio/questio/infra/HaelthycheckController.java
@@ -0,0 +1,22 @@
+package team_questio.questio.infra;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/healthycheck")
+@RequiredArgsConstructor
+public class HaelthycheckController {
+
+    @GetMapping()
+    public ResponseEntity<?> checkGood(){
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+}
